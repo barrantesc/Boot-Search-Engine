03a63cd05418fee65929d4b54856efefd183ff93
playground <connie.s.barrantes@gmail.com>
2022-04-01 23:18:31 -0400

    fixed server.js, install dependencies

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const app = express();
 
 const server = new ApolloServer({
